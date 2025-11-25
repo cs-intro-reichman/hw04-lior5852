0a6a4e7ed5140718e653e935a45a0a6d392b3dfa
lior5852 <lior.tzur@post.runi.ac.il>
2025-11-25 13:01:14 +0200

    finito

--- Primes.java
+++ Primes.java
@@ -12,12 +12,10 @@ public class Primes {
 
         boolean[] isPrime = new boolean[n + 1];
 
-        // initialize
         for (int i = 2; i <= n; i++) {
             isPrime[i] = true;
         }
 
-        // sieve
         for (int p = 2; p * p <= n; p++) {
             if (isPrime[p]) {
                 for (int k = p * p; k <= n; k += p) {
@@ -28,7 +26,6 @@ public class Primes {
 
         int count = 0;
 
-        // print primes
         for (int i = 2; i <= n; i++) {
             if (isPrime[i]) {
                 System.out.println(i);
@@ -36,7 +33,6 @@ public class Primes {
             }
         }
 
-        // percentage (floor)
         int percent = (count * 100) / n;
 
         System.out.println("There are " + count + " primes between 2 and " + n +
